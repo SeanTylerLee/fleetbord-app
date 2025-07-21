cb551d162b1e3272ee1188fe12b16602a3950e0a
SeanTylerLee <217217368+SeanTylerLee@users.noreply.github.com>
2025-07-20 22:10:19 -0500

    c

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moneymaster-v1';
+const CACHE_NAME = 'moneymaster-v2';
 const URLS_TO_CACHE = [
   './',
   './index.html',
@@ -11,14 +11,15 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 

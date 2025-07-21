7e1fbe9c27a6a7dd4b0717ebd512283c05b1f087
SeanTylerLee <217217368+SeanTylerLee@users.noreply.github.com>
2025-07-20 22:01:43 -0500

    c

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,29 @@
-const CACHE_NAME = 'pwa-app-v1';
+const CACHE_NAME = 'moneymaster-v1';
 const URLS_TO_CACHE = [
   './',
   './index.html',
+  './income.html',
+  './bills.html',
+  './spending.html',
+  './settings.html',
   './styles.css',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(res => res || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
